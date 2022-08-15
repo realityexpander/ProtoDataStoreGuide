c7f6049e7275e06a3f8a8dbc5fc5bd41640c2ca9
Chris Athanas <realityexpander@gmail.com>
2022-08-15 04:55:34 -0500

    initial commit

---
+++ app/src/main/java/com/realityexpander/protodatastoreguide/ui/theme/Theme.kt
@@ -0,0 +1,55 @@
+package com.realityexpander.protodatastoreguide.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ExperimentalGraphicsApi
+
+@OptIn(ExperimentalGraphicsApi::class)
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    onSurface = Color.White,
+    background = Color.hsl(0f, 0f, 0.1f),
+)
+
+@OptIn(ExperimentalGraphicsApi::class)
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200,
+    onSurface = Color.Black,
+    background = Color.hsl(0f, 0f, 0.9f),
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ProtoDataStoreGuideTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
